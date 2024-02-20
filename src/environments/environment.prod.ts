490c05a26fc4a147906960abd439634037b043e4
Matin KHD <matink714@gmail.com>
2024-02-20 15:28:06 +0330

    production ready

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const env = {
+  database: {
+    username: 'matink714',
+    password: 'HUTffvNkjfEbcioe',
+  },
+  JWT_KEY: 'matinanahita',
+  apiUrl: 'http://localhost:3000/api',
+  production: true,
+};
